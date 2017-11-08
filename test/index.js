64a5d191169b0fc663d1689cdf2f91e8d3e478c5
Anthony Graddy <anthony@locationq.com>
2017-11-08 05:03:39 +0000

    Working

--- test/index.js
+++ test/index.js
@@ -1,8 +1,65 @@
-process.chdir('test');
+var path = require('path');
+process.chdir(path.dirname(__filename));
 var tap = require('agraddy.test.tap')(__filename);
 
 var mod = require('../');
 
-tap.assert.equal(mod.main('result'), 'result', 'Should be equal.');
+start();
+
+function start() {
+	var index = 0;
+	mod(function() {
+		return index < 3
+	},
+	function(cb) {
+		index++;
+		cb();
+	},
+	function(err) {
+		tap.assert.equal(index, 3, 'Should be equal.');
+		endEarly();
+	});
+}
+
+function endEarly() {
+	var index = 0;
+	mod(function() {
+		return index < 3
+	},
+	function(cb) {
+		index++;
+		if(index == 2) {
+			cb(new Error('End early'));
+		} else {
+			cb();
+		}
+	},
+	function(err) {
+		tap.assert.equal(err.message, 'End early', 'An error should short circuit.');
+		tap.assert.deepEqual(index, 2, 'Should be equal.');
+		async();
+	});
+}
 
 
+function async() {
+	var index = 0;
+	var order;
+
+	order = 'before';
+	mod(function() {
+		return index < 100
+	},
+	function(cb) {
+		index++;
+		cb();
+	},
+	function(err) {
+		tap.assert.equal(order, 'after', 'Make sure it is actually async.');
+		end();
+	});
+	order = 'after';
+}
+
+function end() {
+}
